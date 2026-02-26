c5c4d3a24b2be0651c4a07796c457e093e89922a
Harshada Sharad Patil <harshadap1204@gmail.com>
2026-02-26 10:52:24 +0530

    Create prob_1.py

---
+++ Python_Challenge/Day_01/prob_1.py
@@ -0,0 +1,12 @@
+# Write a Python program to check whether a number entered by the user is even or odd.
+
+num = int(input("Enter a Number: "))
+if num / 2 == 0:
+  print(f"{num} Is Even")
+else :
+  print(f"{num} Is Odd")
+
+
+# OutPut :
+# Enter a Number: 45
+# 45 Is Odd
