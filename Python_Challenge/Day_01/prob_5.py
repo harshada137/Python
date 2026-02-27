b9a1de26cef7e9216a9d0a9e52acbe47212b350f
Harshada Sharad Patil <harshadap1204@gmail.com>
2026-02-27 21:21:25 +0530

    Update prob_5.py

--- Python_Challenge/Day_01/prob_5.py
+++ Python_Challenge/Day_01/prob_5.py
@@ -1,1 +1,9 @@
 # Write a Python program to find the sum of the first N natural numbers.
+
+n = int(input("Enter a number: "))
+
+sum = 0
+for i in range(1, n + 1):
+    sum += i
+
+print("Sum:", sum)
