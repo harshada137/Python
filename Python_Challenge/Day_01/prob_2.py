00da23be586b40ffa2532ee278eb1df881f8b759
Harshada Sharad Patil <harshadap1204@gmail.com>
2026-02-27 21:18:06 +0530

    Update prob_2.py

--- Python_Challenge/Day_01/prob_2.py
+++ Python_Challenge/Day_01/prob_2.py
@@ -1,1 +1,21 @@
 # Write a Python program to find the largest among three numbers entered by the user.
+
+num1 = int(input("Enter a Number: "))
+num2 = int(input("Enter a Number: "))
+num3 = int(input("Enter a Number: "))
+
+print(num1, num2, num3)
+
+if num1 > num2 and num1> num3:
+    print(f"{num1} Is greatest of all")
+
+elif num2 > num3 and num2 > num3:
+    print(f"{num2} Is greates of all")
+
+elif num3 > num1 and num3 > num2:
+    print(f"{num3} Is greatest of all")
+
+else:
+    print("All Values are Same")        
+
+
