7c47d266fc9e97d419b53f23a31810dae301f282
Harshada Sharad Patil <harshadap1204@gmail.com>
2026-02-27 21:19:46 +0530

    Update prob_3.py

--- Python_Challenge/Day_01/prob_3.py
+++ Python_Challenge/Day_01/prob_3.py
@@ -1,1 +1,9 @@
 # Write a Python program to reverse a given string without using built-in reverse functions.
+
+a = input("Enter a string: ")
+
+reversed_a = ""
+for char in a:
+    reversed_a = char + reversed_a
+
+print("Reversed string:", reversed_a)
