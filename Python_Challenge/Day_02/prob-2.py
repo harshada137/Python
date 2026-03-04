5313d726efaa2bdbd9f54f95757eda6bfcbd0593
Harshada Sharad Patil <harshadap1204@gmail.com>
2026-03-04 16:19:26 +0530

    Update prob-2.py

--- Python_Challenge/Day_02/prob-2.py
+++ Python_Challenge/Day_02/prob-2.py
@@ -1,1 +1,16 @@
 # Write a function to check whether a number is prime.
+n = int(input("Enter a number: "))
+
+if n <= 1:
+    print("Not Prime")
+else:
+    is_prime = True
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            is_prime = False
+            break
+    
+    if is_prime:
+        print("Prime")
+    else:
+        print("Not Prime")
