9b543f429d1b1d906b6bc619ff418bc54d9d5abf
Harshada Sharad Patil <harshadap1204@gmail.com>
2026-03-04 16:19:06 +0530

    Update prob-1.py

--- Python_Challenge/Day_02/prob-1.py
+++ Python_Challenge/Day_02/prob-1.py
@@ -1,1 +1,13 @@
 # Find the second largest number in a list without using sort()
+nums = list(map(int, input("Enter numbers separated by space: ").split()))
+
+largest = second = float('-inf')
+
+for num in nums:
+    if num > largest:
+        second = largest
+        largest = num
+    elif largest > num > second:
+        second = num
+
+print("Second Largest:", second)
