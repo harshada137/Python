525a631bcbfe491c5f72f8528205abe5a7c20435
Harshada Sharad Patil <harshadap1204@gmail.com>
2026-03-07 14:48:41 +0530

    Update prob-5.py

--- Python_Challenge/Day_02/prob-5.py
+++ Python_Challenge/Day_02/prob-5.py
@@ -1,1 +1,15 @@
-# Given a string, count frequency of each character.
+# Count frequency of each character in a string
+
+text = input("Enter a string: ")
+
+freq = {}
+
+for char in text:
+    if char in freq:
+        freq[char] += 1
+    else:
+        freq[char] = 1
+
+print("Character frequencies:")
+for key, value in freq.items():
+    print(key, ":", value)
