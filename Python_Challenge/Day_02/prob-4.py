9687f457f91a308947e796e8bb100cbc090ea6cd
Harshada Sharad Patil <harshadap1204@gmail.com>
2026-03-07 14:47:51 +0530

    Update prob-4.py

--- Python_Challenge/Day_02/prob-4.py
+++ Python_Challenge/Day_02/prob-4.py
@@ -1,1 +1,12 @@
 # Remove duplicates from a list without using set()
+
+numbers = input("Enter numbers separated by space: ")
+
+nums = numbers.split()
+unique_list = []
+
+for num in nums:
+    if num not in unique_list:
+        unique_list.append(num)
+
+print("List without duplicates:", unique_list)
